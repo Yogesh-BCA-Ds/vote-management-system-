e86cdbfc09efa8302e5f02e8f5864324b98c34ba
= <=>
2026-09-20 11:10:35 +0530

    fixing errors and structing the basic structure

--- app.py
+++ app.py
@@ -2,11 +2,28 @@ from flask import *
 
 app = Flask("__name__")
 
-@app.route("/register",method=["POST"])
-def user_based_page():
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+@app.route("/register")
+def register_page():
+    return render_template("register.html")
+
+@app.route("/data",methods=["POST"])
+def data():
     fist_name = request.form["first name"]
     last_name = request.form["last name"]
-    username = request.form["email"]
+    username = request.form["username"]
+    email = request.form["email"]
     password = request.form["password"]
     role = request.form["option"]
-       
+    return jsonify({"message":"successfull",
+                    "fname":first_name,
+                    "lname":last_name,
+                    "user_name":username,
+                    "email":email,
+                    "password":password,
+                    "role":role})
+if __name__ == '__main__':
+    app.run(debug=True)
